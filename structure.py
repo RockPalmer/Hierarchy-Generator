b8fb921e289612f98d022c9028dd924ead330321
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2026-03-18 12:47:35 -0700

    Update structure.py

--- structure.py
+++ structure.py
@@ -1,1 +1,76 @@
-
+class Entity:
+  pass
+class Type(Entity):
+  name : str
+  attributes : set[Attribute]
+class Attribute(Entity):
+  name : str
+class Operation:
+  pass
+class Function(Operation):
+  pass
+class Projection(Function):
+  directional : bool = False
+  parameter = list[str] # length > 0
+  input = Type
+  returns = Type
+class Selection(Function):
+  directional : bool = False
+  parameter = Callable[[],bool]
+  input = Type
+  returns = Type
+class Rename(Function):
+  directional : bool = False
+  parameter : dict[str,str] # length > 0
+structure = {
+  "operator" : {
+    "function" : {
+      "rename" : {
+        "directional" : false,
+        "parameter" : "MAPPING[STRING,STRING]{>0}",
+        "input" : "TYPE"
+      },
+      "alias" : {
+        "directional" : false,
+        "parameter" : "STRING",
+        "input" : "TYPE"
+      },
+      "union" : {
+        "directional" : false,
+        "input" : "TYPE..."
+      },
+      "intersection" : {
+        "directional" : false,
+        "input" : "TYPE..."
+      },
+      "symmetric difference" : {
+        "directional" : false,
+        "input" : "TYPE..."
+      },
+      "difference" : {
+        "directional" : true,
+        "input" : "TYPE..."
+      },
+      "product" : {
+        "directional" : true,
+        "input" : "TYPE..."
+      }
+    },
+    "relationship" : {
+      "inheritance" : {
+        "directional" : true,
+        "input" : [
+          "TYPE",
+          "TYPE"
+        ]
+      },
+      "instance" : {
+        "directional" : true,
+        "input" : [
+          "ATTRIBUTE",
+          "TYPE"
+        ]
+      }
+    }
+  }
+}
