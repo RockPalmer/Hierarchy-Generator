a7539f85c0135f6bb38a2ce4d586cefb3b90efd6
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2026-03-18 12:54:35 -0700

    Refactor structure and add new Function subclasses
    Removed the structure dictionary and added new Function subclasses.

--- structure.py
+++ structure.py
@@ -1,76 +1,78 @@
+from dataclasses import dataclass
+
+@dataclass
 class Entity:
   pass
+@dataclass
 class Type(Entity):
   name : str
   attributes : set[Attribute]
+@dataclass
 class Attribute(Entity):
   name : str
+@dataclass
 class Operation:
   pass
+@dataclass
 class Function(Operation):
   pass
+@dataclass
 class Projection(Function):
   directional : bool = False
   parameter = list[str] # length > 0
   input = Type
   returns = Type
+@dataclass
 class Selection(Function):
   directional : bool = False
   parameter = Callable[[],bool]
   input = Type
   returns = Type
+@dataclass
 class Rename(Function):
   directional : bool = False
   parameter : dict[str,str] # length > 0
-structure = {
-  "operator" : {
-    "function" : {
-      "rename" : {
-        "directional" : false,
-        "parameter" : "MAPPING[STRING,STRING]{>0}",
-        "input" : "TYPE"
-      },
-      "alias" : {
-        "directional" : false,
-        "parameter" : "STRING",
-        "input" : "TYPE"
-      },
-      "union" : {
-        "directional" : false,
-        "input" : "TYPE..."
-      },
-      "intersection" : {
-        "directional" : false,
-        "input" : "TYPE..."
-      },
-      "symmetric difference" : {
-        "directional" : false,
-        "input" : "TYPE..."
-      },
-      "difference" : {
-        "directional" : true,
-        "input" : "TYPE..."
-      },
-      "product" : {
-        "directional" : true,
-        "input" : "TYPE..."
-      }
-    },
-    "relationship" : {
-      "inheritance" : {
-        "directional" : true,
-        "input" : [
-          "TYPE",
-          "TYPE"
-        ]
-      },
-      "instance" : {
-        "directional" : true,
-        "input" : [
-          "ATTRIBUTE",
-          "TYPE"
-        ]
-      }
-    }
-  }
-}
+  input = Type
+  returns = Type
+@dataclass
+class Alias(Function):
+  directional : bool = False
+  parameter = str
+  input = Type
+  returns = Type
+@dataclass
+class Union(Function):
+  directional : bool = False
+  input = set[Type]
+  returns = Type
+@dataclass
+class Intersection(Function):
+  directional : bool = False
+  input = set[Type]
+  returns = Type
+@dataclass
+class SymmetricDifference(Function):
+  directional : bool = False
+  input = set[Type]
+  returns = Type
+@dataclass
+class Difference(Function):
+  directional : bool = True
+  input = list[Type]
+  returns = Type
+@dataclass
+class Product(Function):
+  directional : bool = True
+  input = list[Type]
+  returns = Type
+@dataclass
+class Relationship(Operation):
+  pass
+@dataclass
+class Inheritance(Relationship):
+  directional : bool = True
+  input : tuple[Type,Type]
+@dataclass
+class Instance(Relationship):
+  directional : bool = True
+  input : tuple[Attribute,Type]
